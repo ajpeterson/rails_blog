51b4738642967742c479ec09f5fce479ef9a3035
[01;31m[K"name"[m[K: "Andrew Peterson <ajpeterson79@gmail.com>
2016-07-31 16:40:45 -0700

    Copy controllers over from previous blog attempt.

---
+++ blog/app/controllers/posts_controller.rb
@@ -0,0 +1,52 @@
+class PostsController < ApplicationController
+
+  http_basic_authenticate_with name: 'ajpeterson', password: 'dbcSeattle#1', except: [:index, :show]
+
+  def index
+    @posts = Post.all.order("created_at DESC")
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    redirect_to root_path
+  end
+
+  private
+    def post_params
+      params.require(:post).permit(:title, :thoughts)
+    end
+end
